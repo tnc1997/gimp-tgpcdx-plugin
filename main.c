2c3a37bdcd92df6003a07f9b6cbb683fc8366925
Thomas Clark <tnc1997@outlook.com>
2025-03-23 08:15:34 +0000

    refactor: add plugin define directives

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+#define PLUG_IN_AUTHOR "Thomas Clark"
+#define PLUG_IN_BINARY "file-tgpcdx"
+#define PLUG_IN_COPYRIGHT "Copyright (C) 2025 Thomas Clark. All rights reserved."
+#define PLUG_IN_ROLE "gimp-file-tgpcdx"
+#define PLUG_IN_VERSION "1.0.0"
+
 int main(void) {
   printf("Hello, World!\n");
   return 0;
