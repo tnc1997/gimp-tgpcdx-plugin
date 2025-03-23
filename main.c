febd43d5817943fc6e227201ddbaa3baf3566f43
Thomas Clark <tnc1997@outlook.com>
2025-03-23 09:57:25 +0000

    style: align clang format with gimp

--- main.c
+++ main.c
@@ -5,37 +5,51 @@
 
 #define LOAD_PROC "file-tgpcdx-load"
 
-#define PLUG_IN_AUTHOR "Thomas Clark"
-#define PLUG_IN_BINARY "file-tgpcdx"
-#define PLUG_IN_COPYRIGHT "Copyright (C) 2025 Thomas Clark. All rights reserved."
-#define PLUG_IN_ROLE "gimp-file-tgpcdx"
-#define PLUG_IN_VERSION "1.0.0"
+#define PLUG_IN_AUTHOR    "Thomas Clark"
+#define PLUG_IN_BINARY    "file-tgpcdx"
+#define PLUG_IN_COPYRIGHT "Copyright (C) 2025 Thomas Clark"
+#define PLUG_IN_ROLE      "gimp-file-tgpcdx"
+#define PLUG_IN_VERSION   "1.0.0"
 
-static void query(void);
+static void query (void);
 
-static void run(const gchar *name, gint nparams, const GimpParam *param,
-                gint *nreturn_vals, GimpParam **return_vals);
+static void run (const gchar     *name,
+                 gint             nparams,
+                 const GimpParam *param,
+                 gint            *nreturn_vals,
+                 GimpParam      **return_vals);
 
-const GimpPlugInInfo PLUG_IN_INFO = {NULL, NULL, query, run};
+const GimpPlugInInfo PLUG_IN_INFO = { NULL, NULL, query, run };
 
 static GimpParamDef load_args[] = {
-    {GIMP_PDB_INT32, "run_mode", "Interactive, non-interactive"},
-    {GIMP_PDB_STRING, "filename", "The name of the file to load"},
-    {GIMP_PDB_STRING, "raw_filename", "The name entered"},
-    {GIMP_PDB_INT32, "load_mipmaps", "Load mipmaps if present"}};
+  { GIMP_PDB_INT32,  "run_mode",     "Interactive, non-interactive" },
+  { GIMP_PDB_STRING, "filename",     "The name of the file to load" },
+  { GIMP_PDB_STRING, "raw_filename", "The name entered"             },
+  { GIMP_PDB_INT32,  "load_mipmaps", "Load mipmaps if present"      }
+};
 
 static GimpParamDef load_return_vals[] = {
-    {GIMP_PDB_IMAGE, "image", "Output image"}};
+  { GIMP_PDB_IMAGE, "image", "Output image" }
+};
 
-MAIN()
+MAIN ()
 
-static void query(void) {
-  gimp_install_procedure(LOAD_PROC, "Loads files in TGPCDX image format.",
-                         "Loads files in TGPCDX image format.", PLUG_IN_AUTHOR,
-                         PLUG_IN_COPYRIGHT, PLUG_IN_VERSION, "TGPCDX image",
-                         NULL, GIMP_PLUGIN, G_N_ELEMENTS(load_args),
-                         G_N_ELEMENTS(load_return_vals), load_args,
-                         load_return_vals);
+static void
+query (void)
+{
+  gimp_install_procedure (LOAD_PROC,
+                          "Loads files in TGPCDX image format",
+                          "Loads files in TGPCDX image format",
+                          PLUG_IN_AUTHOR,
+                          PLUG_IN_COPYRIGHT,
+                          PLUG_IN_VERSION,
+                          "TGPCDX image",
+                          NULL,
+                          GIMP_PLUGIN,
+                          G_N_ELEMENTS (load_args),
+                          G_N_ELEMENTS (load_return_vals),
+                          load_args,
+                          load_return_vals);
 
-  gimp_register_load_handler(LOAD_PROC, "tgpcdx", NULL);
+  gimp_register_load_handler (LOAD_PROC, "tgpcdx", NULL);
 }
