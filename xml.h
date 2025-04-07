b950a6379c51651ef208e66ff0507cf68f149195
Thomas Clark <tnc1997@outlook.com>
2025-04-07 18:13:53 +0100

    refactor(xml): add attribute type definition and structure

--- xml.h
+++ xml.h
@@ -1,7 +1,15 @@
 #ifndef XML_H
 #define XML_H
 
+typedef struct _XmlAttribute XmlAttribute;
+
 typedef struct _XmlName XmlName;
+
+struct _XmlAttribute
+{
+  XmlName *name;
+  char    *value;
+};
 
 struct _XmlName
 {
